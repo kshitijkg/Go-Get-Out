396f40835d7444c5867576f53fbc780a1405b365
Satwik Singh <satwik@Satwiks-MacBook-Pro.local>
2017-12-03 15:18:28 -0600

    UI done

---
+++ app/src/main/java/com/example/kshit/go_get_out/ActivityScreen.java
@@ -0,0 +1,22 @@
+package com.example.kshit.go_get_out;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+
+import java.util.List;
+
+public class ActivityScreen extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_screen);
+        String[] ListofStrings = {"String1","String2","String3","String4"};
+        ListView history = (ListView) findViewById(R.id.List1);
+        ListAdapter historyAdapter  new ArrayAdapter<String>(this, R.id.List1, ListofStrings )
+    }
+}

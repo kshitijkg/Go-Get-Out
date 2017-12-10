422d750e96a6ae1cb106f36500a9fd10d3165b4b
wqyjason <31679047+wqyjason@users.noreply.github.com>
2017-12-10 16:43:46 -0600

    GPS done

--- app/src/main/java/com/example/kshit/go_get_out/ActivityScreen.java
+++ app/src/main/java/com/example/kshit/go_get_out/ActivityScreen.java
@@ -1,8 +1,11 @@
 package com.example.kshit.go_get_out;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListAdapter;
 import android.widget.ListView;
 
@@ -17,6 +20,14 @@ public class ActivityScreen extends AppCompatActivity {
         setContentView(R.layout.activity_screen);
         String[] ListofStrings = {"String1","String2","String3","String4"};
         ListView history = (ListView) findViewById(R.id.List1);
-        ListAdapter historyAdapter  new ArrayAdapter<String>(this, R.id.List1, ListofStrings )
+        ListAdapter historyAdapter =  new ArrayAdapter<String>(this, R.id.List1, ListofStrings );
+        Button next = (Button)findViewById(R.id.tadayAct);
+        next.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                Intent myIntent = new Intent(view.getContext(), MapsActivity.class);
+                startActivityForResult(myIntent, 0);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/kshit/go_get_out/FinishActivity.java
+++ app/src/main/java/com/example/kshit/go_get_out/FinishActivity.java
@@ -1,11 +1,13 @@
 package com.example.kshit.go_get_out;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 
 public class FinishActivity extends AppCompatActivity {
 
@@ -15,6 +17,7 @@ public class FinishActivity extends AppCompatActivity {
         setContentView(R.layout.activity_finish);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        Button next = (Button)findViewById(R.id.button);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -24,6 +27,16 @@ public class FinishActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        next.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                Intent myIntent = new Intent(view.getContext(), ActivityScreen.class);
+                startActivityForResult(myIntent, 0);
+            }
+        });
+
+
     }
 
 }

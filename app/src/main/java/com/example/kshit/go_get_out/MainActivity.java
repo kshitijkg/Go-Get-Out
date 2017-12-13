86bbee03b5da3e9f8869b260a8460aa34ac95181
wqyjason <31679047+wqyjason@users.noreply.github.com>
2017-12-13 15:30:23 -0600

    combined

--- app/src/main/java/com/example/kshit/go_get_out/MainActivity.java
+++ app/src/main/java/com/example/kshit/go_get_out/MainActivity.java
@@ -1,25 +1,180 @@
 package com.example.kshit.go_get_out;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
-import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
+
+    SignInButton button;
+    FirebaseAuth mAuth;
+    private final static int RC_SIGN_IN = 2;
+    private final String TAG = "MainActivity";
+    FirebaseAuth.AuthStateListener mAuthListener;
+
+    GoogleApiClient mGoogleApiClient;
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+    DatabaseReference myRef = database.getReference();
+
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button next = (Button)findViewById(R.id.button2);
-        next.setOnClickListener(new View.OnClickListener(){
+        button =(SignInButton) findViewById(R.id.googleBtn);
+        mAuth = FirebaseAuth.getInstance();
+
+        button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent myIntent = new Intent(view.getContext(), ActivityScreen.class);
-                startActivityForResult(myIntent, 0);
+                signIn();
             }
         });
+
+
+
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                if(firebaseAuth.getCurrentUser()!=null){
+                    // This class will be the one from the activity that the user is
+                    // redirected to if the user signs in
+                    startActivity(new Intent(MainActivity.this, ActivityScreen.class));
+                }
+            }
+        };
+
+        // Configure Google Sign In
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this, new GoogleApiClient.OnConnectionFailedListener() {
+                    @Override
+                    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+                        Toast.makeText(MainActivity.this, "Something went wrong", Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .build();
+    }
+
+
+
+    private void signIn() {
+        Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            if(result.isSuccess()){
+                GoogleSignInAccount account = result.getSignInAccount();
+                firebaseAuthWithGoogle(account);
+            }
+            else{
+                Toast.makeText(MainActivity.this, "Auth went wrong", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    private void firebaseAuthWithGoogle(GoogleSignInAccount account) {
+        AuthCredential credential = GoogleAuthProvider.getCredential(account.getIdToken(), null);
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d("TAG", "signInWithCredential:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            final DatabaseReference newRef = myRef.child("Users").push();
+                            newRef.child("Name").setValue(user.getDisplayName());
+                            final DatabaseReference ExerciseRef = newRef.child("Exercises");
+                            final DatabaseReference CurRef = ExerciseRef.child("Current");
+                            final DatabaseReference PreRef = ExerciseRef.child("Previous");
+                            CurRef.child("Name").setValue("A");
+                            CurRef.child("Start Point").setValue("B");
+                            CurRef.child("End Point").setValue("B");
+                            CurRef.child("Distance").setValue(5);
+                            CurRef.child("Start Time").setValue(3);
+                            CurRef.child("End Time").setValue(5);
+                            DatabaseReference newPreRef1 = ExerciseRef.child("Previous").child("Date2");
+                            newPreRef1.child("Name").setValue("");
+                            newPreRef1.child("Start Point").setValue("");
+                            newPreRef1.child("End Point").setValue("");
+                            newPreRef1.child("Speed").setValue("");
+                            newPreRef1.child("Distance").setValue("");
+                            newPreRef1.child("Time").setValue("");
+                            newRef.addListenerForSingleValueEvent(new ValueEventListener() {
+                                @Override
+                                public void onDataChange(DataSnapshot dataSnapshot) {
+                                    if ((dataSnapshot.child("Exercises").child("Current").child("End Time").getValue().toString())!=null){
+                                        DatabaseReference newPreRef = ExerciseRef.child("Previous").child("Date");
+                                        DataSnapshot currRef = dataSnapshot.child("Exercises").child("Current");
+                                        newPreRef.child("Name").setValue(currRef.child("Name").getValue().toString());
+                                        newPreRef.child("Start Point").setValue(currRef.child("Start Point").getValue());
+                                        newPreRef.child("End Point").setValue(currRef.child("End Point").getValue());
+                                        newPreRef.child("Speed").setValue("");
+                                        newPreRef.child("Distance").setValue("");
+                                        newPreRef.child("Time").setValue("");
+                                        dataSnapshot.child("Exercises").child("Current").getRef().removeValue();
+                                    }
+
+                                }
+                                @Override
+                                public void onCancelled(DatabaseError databaseError) {
+
+                                }
+                            });
+
+                            //updateUI(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w("TAG", "signInWithCredential:failure", task.getException());
+                            Toast.makeText(MainActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                            //updateUI(null);
+                        }
+                    }
+                });
     }
 }
